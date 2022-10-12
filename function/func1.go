30999beac4950217916b206af1a2737abbf016d5
Rosaldo Garcia <rosaldo.garcia@hcl.com>
2022-10-12 23:19:16 +0800

    adding function

---
+++ function/func1.go
@@ -0,0 +1,19 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	foo()
+	bar("Ross")
+}
+
+func foo() {
+	fmt.Println("Hello, World")
+}
+
+func bar(s string) {
+	fmt.Println("Welcome to bar", s)
+}
+
