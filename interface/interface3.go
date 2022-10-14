d458c7bf8bd5074f70a5191f065de7c3568bbbde
Rosaldo Garcia <rosaldo.garcia@hcl.com>
2022-10-14 12:44:30 +0800

    program composition

---
+++ interface/interface3.go
@@ -0,0 +1,18 @@
+// https://go.dev/play/p/OjEy6yqdZNQ
+
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+type people struct{}
+
+func (p people) speak(){}
+
+type human interface{}
+
+func bar(h human){}
+
+func main() {}
+
